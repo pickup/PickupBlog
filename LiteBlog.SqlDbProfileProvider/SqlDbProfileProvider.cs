4411f165383311c825ff571bf29bf32c93e70c41
Dongye Zhang <zdy_bit@163.com>
2012-12-18 17:31:24 +0800

    添加Sql server 存储引擎，仅建立了空项目

---
+++ LiteBlog.SqlDbProfileProvider/SqlDbProfileProvider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web.Profile;
+using System.Xml;
+
+using LiteBlog.Common;
+
+namespace LiteBlog.SqlDbProfileProvider
+{
+    public class SqlDbProfileProvider : ProfileProvider
+    {
+        public override int DeleteInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DeleteProfiles(string[] usernames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int DeleteProfiles(ProfileInfoCollection profiles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection FindInactiveProfilesByUserName(ProfileAuthenticationOption authenticationOption, string usernameToMatch, DateTime userInactiveSinceDate, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection FindProfilesByUserName(ProfileAuthenticationOption authenticationOption, string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection GetAllInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProfileInfoCollection GetAllProfiles(ProfileAuthenticationOption authenticationOption, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LiteBlog.SqlDbProfileProvider/sql/UserInfo.sql
@@ -0,0 +1,46 @@
+﻿USE [LiteBlogDB]
+GO
+
+/****** Object:  Table [dbo].[UserInfo]    Script Date: 12/18/2012 17:16:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserInfo](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+	[Locked] [smallint] NOT NULL,
+	[CreatedTime] [datetime] NULL,
+	[LastLoginTime] [datetime] NULL,
+	[LastActivityTime] [datetime] NULL,
+	[LastPasswordTime] [datetime] NULL,
+	[LastLockedTime] [datetime] NULL,
+ CONSTRAINT [PK_UserInfo] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_Locked]  DEFAULT ((0)) FOR [Locked]
+GO
+
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_CreatedTime]  DEFAULT (getdate()) FOR [CreatedTime]
+GO
+
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_LastLoginTime]  DEFAULT (getdate()) FOR [LastLoginTime]
+GO
+
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_LastActivityTime]  DEFAULT (getdate()) FOR [LastActivityTime]
+GO
+
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_LastPasswordTime]  DEFAULT (getdate()) FOR [LastPasswordTime]
+GO
+
+ALTER TABLE [dbo].[UserInfo] ADD  CONSTRAINT [DF_UserInfo_LastLockedTime]  DEFAULT (getdate()) FOR [LastLockedTime]
+GO
