a8e0dce337497c663fc8d4a5a4d038a18bc9d1aa
Dongye Zhang <zdy_bit@163.com>
2012-12-21 17:20:23 +0800

    添加了EF5.0组件

---
+++ LiteBlog.SqlDbProfileProvider/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace LiteBlog.SqlDbProfileProvider
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext()
+            : base() { }
+
+        public UserDbContext(string name)
+            : base(name) { }
+
+
+    }
+}
