ae3729a551f7e3592418148295feb512c6537333
Dongye Zhang <zdy_bit@163.com>
2012-12-25 23:15:19 +0800

    部分实现SqlDbProfileProvider

--- LiteBlog.SqlDbProfileProvider/SqlDbProfileProvider.cs
+++ LiteBlog.SqlDbProfileProvider/SqlDbProfileProvider.cs
@@ -14,27 +14,86 @@ namespace LiteBlog.SqlDbProfileProvider
 {
     public class SqlDbProfileProvider : ProfileProvider
     {
+        #region Fields
+
+        /// <summary>
+        /// The _tzi.
+        /// </summary>
+        private TimeZoneInfo _tzi;
+
+        /// <summary>
+        /// The app name.
+        /// </summary>
+        private string appName = string.Empty;
+
+        /// <summary>
+        /// The date format.
+        /// </summary>
+        private string dateFormat = "MM/dd/yyyy hh:mm tt";
+
+        private UserDbContext dbContext = new UserDbContext();
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Gets or sets the application name.
+        /// </summary>
+        public override string ApplicationName
+        {
+            get
+            {
+                return this.appName;
+            }
+
+            set
+            {
+                this.appName = value;
+            }
+        }
+        #endregion
+
+        #region Public Methods and Operators
         public override int DeleteInactiveProfiles(ProfileAuthenticationOption authenticationOption, DateTime userInactiveSinceDate)
         {
-            throw new NotImplementedException();
+            var users = dbContext.UserSet.Where(u => u.LastActivityTime < userInactiveSinceDate);
+            foreach (var user in users)
+            {
+                dbContext.UserSet.Remove(user);
+            }
+            return dbContext.SaveChanges();
         }
 
         public override int DeleteProfiles(string[] usernames)
         {
-            throw new NotImplementedException();
+            var users = dbContext.UserSet.Where(u => usernames.Contains(u.Name));
+            foreach (var user in users)
+            {
+                dbContext.UserSet.Remove(user);
+            }
+            return dbContext.SaveChanges();
         }
 
         public override int DeleteProfiles(ProfileInfoCollection profiles)
         {
-            throw new NotImplementedException();
+            List<string> userNames = new List<string>();
+            foreach (ProfileInfo profile in profiles)
+            {
+                userNames.Add(profile.UserName);
+            }
+
+            return this.DeleteProfiles(userNames.Distinct<string>().ToArray<string>());
         }
 
-        public override ProfileInfoCollection FindInactiveProfilesByUserName(ProfileAuthenticationOption authenticationOption, string usernameToMatch, DateTime userInactiveSinceDate, int pageIndex, int pageSize, out int totalRecords)
+        public override ProfileInfoCollection FindInactiveProfilesByUserName(
+            ProfileAuthenticationOption authenticationOption, string usernameToMatch, 
+            DateTime userInactiveSinceDate, int pageIndex, int pageSize, out int totalRecords)
         {
             throw new NotImplementedException();
         }
 
-        public override ProfileInfoCollection FindProfilesByUserName(ProfileAuthenticationOption authenticationOption, string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        public override ProfileInfoCollection FindProfilesByUserName(
+            ProfileAuthenticationOption authenticationOption, string usernameToMatch, 
+            int pageIndex, int pageSize, out int totalRecords)
         {
             throw new NotImplementedException();
         }
@@ -54,18 +113,6 @@ namespace LiteBlog.SqlDbProfileProvider
             throw new NotImplementedException();
         }
 
-        public override string ApplicationName
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
         public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection collection)
         {
             throw new NotImplementedException();
@@ -75,5 +122,6 @@ namespace LiteBlog.SqlDbProfileProvider
         {
             throw new NotImplementedException();
         }
+        #endregion
     }
 }
--- LiteBlog.SqlDbProfileProvider/UserDbContext.cs
+++ LiteBlog.SqlDbProfileProvider/UserDbContext.cs
@@ -6,6 +6,20 @@ using System.Data.Entity;
 
 namespace LiteBlog.SqlDbProfileProvider
 {
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public short Locked { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? LastLoginTime { get; set; }
+        public DateTime? LastActivityTime { get; set; }
+        public DateTime? LastPasswordTime { get; set; }
+        public DateTime? LastLockedTime { get; set; }
+    }
+
     public class UserDbContext : DbContext
     {
         public UserDbContext()
@@ -14,6 +28,14 @@ namespace LiteBlog.SqlDbProfileProvider
         public UserDbContext(string name)
             : base(name) { }
 
+        public DbSet<User> UserSet { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("UserInfo");
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
